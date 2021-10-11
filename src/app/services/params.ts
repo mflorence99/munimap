16a79a9aef89bdfa15d804c680efe2f3c9a2edc7
Mark Florence <mflo999@gmail.com>
2021-10-11 03:57:46 -0400

    Gave up on leaflet

--- src/app/services/params.ts
+++ src/app/services/params.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class Params {
+  esri = {
+    // 👇 don't panic! domain protected
+    apiKey:
+      'AAPKae64696be2e34f6aac7bced46726f427ZCnEEvKPioJi8X0wsYXk0P2I5q1lmQFG5xHmzbj-C4HavKlxv5n7N178jyum_SBh'
+  };
+
   google = {
     // 👇 don't panic! domain protected
     apiKey: 'AIzaSyCAYavpwIUZOayj72XA3AZYJeYjlVscqvk'
