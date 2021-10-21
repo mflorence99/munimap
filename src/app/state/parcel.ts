53c28c779468a82d0b51914e5cfeb5d2a448ca64
Mark Florence <mflo999@gmail.com>
2021-10-21 16:06:57 -0400

    Get ready for parcels from other tiwns

--- src/app/state/parcel.ts
+++ src/app/state/parcel.ts
@@ -1,24 +1,49 @@
 export interface Parcel {
-  abutters: string[];
-  address: string;
+  abutters?: string[];
+  address?: string;
   area: number;
   areaComputed: number;
-  building$: number;
-  cu$: number;
-  elevation: number;
+  building$?: number;
+  callout?: number[] /* 👈 legacy support */;
+  center?: number[] /* 👈 legacy support */;
+  class: string;
+  county: string;
+  cu$?: number /* 👈 should be feature!! */;
+  elevation?: number;
   id: string;
-  // 👉 legacy support
-  label?: { rotate: boolean; split: boolean };
-  land$: number;
-  lengths: number[];
-  minWidth: number;
-  neighborhood: 'U' | 'V' | 'W';
-  orientation: number;
-  owner: string;
-  perimeter: number;
-  sqarcity: number;
-  taxed$: number;
-  usage: '110' | '120' | '190' | '260' | '300' | '400' | '500' | '501' | '502';
-  use: 'CUFL' | 'CUMH' | 'CUMW' | 'CUUH' | 'CUUW' | 'CUWL';
+  label?: { rotate: boolean; split: boolean } /* 👈 legacy support */;
+  land$?: number;
+  lengths?: number[];
+  minWidth?: number;
+  neighborhood?: 'U' | 'V' | 'W';
+  orientation?: number;
+  owner?: string;
+  perimeter?: number;
+  sqarcity?: number;
+  taxed$?: number;
+  town: string;
+  usage:
+    | '110' // Single family residence
+    | '120' // Multi family residence
+    | '130' // Other residewntial
+    | '190' // Current use
+    | '260' // Commercial / Industrial
+    | '300' // Town property
+    | '400' // State property
+    | '500' // State park
+    | '501' // Towm forest
+    | '502'; // Conservation land
+  use?:
+    | 'CUDE' // Discretionary
+    | 'CUFL' // Farm land
+    | 'CUMH' // Managed hardwood
+    | 'CUMO' // Managesd other
+    | 'CUMW' // Managed pine
+    | 'CUNS' // Xmas tree
+    | 'CUUH' // Unmanaged hardwood
+    | 'CUUL' // Unproductive
+    | 'CUUO' // Unm,anaged other
+    | 'CUUW' // Unmanaged pine
+    | 'CUWL'; // Wetland
   zone: string;
 }
