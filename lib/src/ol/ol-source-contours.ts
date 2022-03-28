c5c3f6dd974650830e36357c5d3e1b65a067201e
Mark Florence <mflo999@gmail.com>
2022-03-28 11:50:44 -0400

    Fallback on contours as the preferred service often throws 503 errors

--- lib/src/ol/ol-source-contours.ts
+++ lib/src/ol/ol-source-contours.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import { OLLayerTileComponent } from './ol-layer-tile';
 import { OLMapComponent } from './ol-map';
 
@@ -5,7 +7,12 @@ import { environment } from '../environment';
 
 import { ChangeDetectionStrategy } from '@angular/core';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
 import { Input } from '@angular/core';
+
+import { catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import OLImageTile from 'ol/ImageTile';
 import OLTileWMS from 'ol/source/TileWMS';
@@ -27,17 +34,24 @@ export class OLSourceContoursComponent {
 
   olTileWMS: OLTileWMS;
 
-  url =
-    'https://carto.nationalmap.gov/arcgis/rest/services/contours/MapServer/export?bbox=XXXXXX&bboxSR=102100&imageSR=102100&size=256,256&dpi=96&format=png32&transparent=true&layers=show:YYYYYY&f=image';
+  // 👇 the preferred source for contours us best because it
+  //    includes an elevation annotation -- but it is unreliable,
+  //    often throwing 503 or 504 errors
+
+  urlFallback =
+    'https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage?f=image&format=jpgpng&renderingRule=YYYYYY&bbox=XXXXXX&imageSR=102100&bboxSR=102100&size=256,256';
+
+  urlPreferred =
+    'https://carto.nationalmap.gov/arcgis/rest/services/contours/MapServer/export?bbox=XXXXXX&bboxSR=102100&imageSR=102100&size=256,256&dpi=96&format=png32&transparent=true&layers=show:ZZZZZZ&f=image';
 
   constructor(
+    private http: HttpClient,
     private layer: OLLayerTileComponent,
     private map: OLMapComponent
   ) {
     this.olTileWMS = new OLTileWMS({
       attributions: [attribution],
       crossOrigin: 'anonymous',
-      // eslint-disable-next-line @typescript-eslint/naming-convention
       params: { LAYERS: 'dummy' },
       tileLoadFunction: this.#loader.bind(this),
       url: 'http://dummy.com'
@@ -45,15 +59,47 @@ export class OLSourceContoursComponent {
     this.layer.olLayer.setSource(this.olTileWMS);
   }
 
+  // 👇 all this kind of works because either none of the preferred
+  //    tile loads will fail, or else all of them will
+
   #loader(tile: OLImageTile, src: string): void {
     const img = tile.getImage() as HTMLImageElement;
+    const url = this.#makeURL(src, this.urlPreferred);
+    this.http
+      .get(url, { observe: 'response', responseType: 'blob' })
+      .pipe(
+        catchError(() => {
+          // 👉 the fallback contours are WAAY to heavy
+          this.layer.olLayer.setOpacity(0.33);
+          const url = this.#makeURL(src, this.urlFallback);
+          return this.http.get(url, {
+            observe: 'response',
+            responseType: 'blob'
+          });
+        }),
+        map((response: HttpResponse<Blob>) =>
+          URL.createObjectURL(response.body)
+        )
+      )
+      .subscribe((url: string) => {
+        img.src = url;
+      });
+  }
+
+  #makeURL(src: string, model: string): string {
     const parsed = new URL(src);
     const bbox = parsed.searchParams.get('BBOX');
-    const url = `${
+    const renderingRule = {
+      rasterFunction: 'Contour 25',
+      rasterFunctionArguments: {}
+    };
+    return `${
       environment.endpoints.proxy
     }/proxy/contours?url=${encodeURIComponent(
-      this.url.replace('XXXXXX', bbox).replace('YYYYYY', this.layers.join(','))
+      model
+        .replace('XXXXXX', bbox)
+        .replace('YYYYYY', JSON.stringify(renderingRule))
+        .replace('ZZZZZZ', this.layers.join(','))
     )}`;
-    img.src = url;
   }
 }
