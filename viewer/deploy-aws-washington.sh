3afb335997218805be8652cc27bc04902803a423
Mark Florence <mflo999@gmail.com>
2022-11-13 08:47:51 -0500

    Prepare new version for release

--- viewer/deploy-aws-washington.sh
+++ viewer/deploy-aws-washington.sh
@@ -15,6 +15,6 @@ git push origin main
 
 npm run build:viewer:washington
 
-serverx-angular --app dist/viewer --deploy viewer/aws-washington.json
+# serverx-angular --app dist/viewer --deploy viewer/aws-washington.json
 serverx-angular --app dist/viewer --deploy viewer/aws-washington-dpw.json
-serverx-angular --app dist/viewer --deploy viewer/aws-florence.json
+# serverx-angular --app dist/viewer --deploy viewer/aws-florence.json
