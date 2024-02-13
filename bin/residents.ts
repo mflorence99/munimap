508a5db2d0451fcdd7518a48841736098ca1ca3a
Mark Florence <mflo999@gmail.com>
2024-02-13 09:02:07 -0500

    Sync point

---
+++ bin/residents.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs';
+import { writeFileSync } from 'fs';
+
+const geojson = JSON.parse(
+  readFileSync('./proxy/assets/washington-parcels.geojson').toString()
+);
+
+const lines = geojson.features
+  .filter(
+    (feature) =>
+      feature.properties.ownership === 'R' && feature.properties.usage === '110'
+  )
+  .sort((p, q) =>
+    sortaddr(p.properties.address).localeCompare(sortaddr(q.properties.address))
+  )
+  .reduce(
+    (acc, feature) =>
+      (acc += `${feature.properties.address}\t${feature.properties.owner}\t ${feature.id}\n`),
+    ''
+  );
+
+writeFileSync('/home/mflo/Downloads/residents.csv', lines);
+
+function sortaddr(addr: string): string {
+  const parts = addr.split(' ');
+  const [num, ...rest] = parts;
+  if (isNaN(+num)) return addr;
+  else return [...rest, num.padStart(5, '0')].join(' ');
+}
