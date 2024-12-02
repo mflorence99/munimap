e189a959f05c53b5e0df8c7a614d68621d294960
Mark Florence <mflo999@gmail.com>
2024-12-02 07:51:33 -0500

    ESLint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,19 @@
 import myConfig from 'eslint-config-mflorence99';
 
-export default myConfig;
+export default [
+  ...myConfig,
+  {
+    ignores: ['**/*.mjs'],
+    languageOptions: {
+      parserOptions: {
+        project: [
+          'author/src/tsconfig.json',
+          'bin/tsconfig.json',
+          'proxy/src/tsconfig.json',
+          'viewer/src/tsconfig.json',
+          'worker/src/tsconfig.json'
+        ]
+      }
+    }
+  }
+];
