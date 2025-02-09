62ab76f43b3c42ce6cd2a4079e5c943b6c699a17
Mark Florence <mflo999@gmail.com>
2025-02-09 13:00:05 -0500

    Prepare new version for release

--- bin/deploy-aws-viewer-washington.sh
+++ bin/deploy-aws-viewer-washington.sh
@@ -4,8 +4,8 @@ bin/bump-version.sh
 
 npm run build:viewer:washington
 
-# serverx-angular --app dist/viewer --deploy viewer/aws-washington.json
-# serverx-angular --app dist/viewer --deploy viewer/aws-washington-dpw.json
+serverx-angular --app dist/viewer --deploy viewer/aws-washington.json
+serverx-angular --app dist/viewer --deploy viewer/aws-washington-dpw.json
 # serverx-angular --app dist/viewer --deploy viewer/aws-florence.json
 # serverx-angular --app dist/viewer --deploy viewer/aws-tcv-library.json
 serverx-angular --app dist/viewer --deploy viewer/aws-apdvd.json
